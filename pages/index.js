6ebfe6e22d44d14915cd07f4a9ac787a327611a6
Pascal GAULT <pascal@inrage.fr>
2021-12-21 00:57:40 +0100

    feat(hp): add TMA block on the frontpage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import SectionTitle from '../components/SectionTitle';
 import PrestationItem from '../components/items/PrestationItem';
 import ImagePrestationBuildWebsite from '../public/images/prestations/creation-site-internet.png';
 import ImagePrestationTma from '../public/images/prestations/infogerance-tma-maintenance.png';
 import ImagePrestationHosting from '../public/images/prestations/hebergement-optimise-rapide.png';
+import Diagonal from '../components/layouts/Diagonal';
+import ImageDiscoverTma from '../public/images/prestations/presentation-integration-web.jpeg';
 
 export default function Home() {
   return (
@@ -65,6 +69,42 @@ export default function Home() {
             pour vous proposer l’offre idéale pour la nature de votre site web.
           </PrestationItem>
         </div>
+      </div>
+
+      <div className="relative">
+        <Diagonal
+          className="-z-10"
+          flipX
+          flipY
+          bgClass="fill-gray-dark"
+          bgCorner="fill-orange"
+        />
+        <Image layout="fill" objectFit="cover" className="-z-10" src={ImageDiscoverTma} alt="Prestation de maintenance TMA" />
+        <div className="container relative -my-10">
+          <div className="w-1/2">
+            <h2 className="text-3xl text-white font-medium mb-3">
+              Besoin d’une estimation de votre site
+              <br />
+              et/ou de sa maintenance
+            </h2>
+            <p>Simple et en toute transparence, n’hésitez pas à me demander une estimation gratuite pour la création de votre site web ou de la maintenance applicative.</p>
+            <p>Vous recevrez dans la journée votre devis et nous pourrons prendre contact pour définir ensemble les tâches.</p>
+
+            <div className="flex flex-wrap flex-start">
+              <Link href="/">
+                <a className="button mt-3 mr-2">Demandez votre devis maintenant</a>
+              </Link>
+              <Link href="/">
+                <a className="button-outline mt-3">Mes offres de maintenance</a>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <Diagonal
+          className="-z-10"
+          bgClass="fill-gray-dark"
+          bgCorner="fill-orange"
+        />
       </div>
     </div>
   );
