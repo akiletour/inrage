61d7ebed4b7df1d209b441396a3698ed61ad85f3
Pascal GAULT <pascal@inrage.fr>
2021-12-21 10:29:14 +0100

    fix(hp): change bg of project items and add diagonal CTA

--- components/layouts/Diagonal.js
+++ components/layouts/Diagonal.js
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { Fragment } from 'react';
 
 export default function Diagonal({
-  className, flipX, flipY, bgClass, bgCorner, preserveRatio,
+  className, flipX, flipY, bgClass, bgCorner, preserveRatio, cta,
 }) {
   const classeNames = [`${className} w-full block`];
 
@@ -14,16 +16,31 @@ export default function Diagonal({
   }
 
   return (
-    <svg
-      className={classeNames.join(' ')}
-      height="361"
-      preserveAspectRatio={preserveRatio ? 'xMinYMin meet' : 'xMidYMax slice'}
-      viewBox="0 0 1920 361"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path className={bgClass} d="M1349 234L1920 0v361H0z" fill="#000" />
-      <path className={bgCorner} d="M1445 197.5L1920 4v249z" fill="#BE4C4C" />
-    </svg>
+    <div className="relative">
+      <svg
+        className={classeNames.join(' ')}
+        height="361"
+        preserveAspectRatio={preserveRatio ? 'xMinYMin meet' : 'xMidYMax slice'}
+        viewBox="0 0 1920 361"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path className={bgClass} d="M1349 234L1920 0v361H0z" fill="#000" />
+        <path className={bgCorner} d="M1445 197.5L1920 4v249z" fill="#BE4C4C" />
+      </svg>
+
+      {cta?.href && (
+      <Link href={cta.href}>
+        <a className="absolute left-1/2 bottom-3 -translate-x-1/2 w-20 h-20 bg-gray-dark text-gray hover:text-white rounded-full flex flex-col items-center pt-3">
+          <div>{cta.icon}</div>
+          <div className="mt-1 text-sm text-center leading-4">
+            {Array.isArray(cta.title)
+              ? cta.title.map((ttl) => <Fragment key={ttl}>{ttl}</Fragment>)
+              : <>ttl</>}
+          </div>
+        </a>
+      </Link>
+      )}
+    </div>
   );
 }
 
@@ -33,6 +50,14 @@ Diagonal.propTypes = {
   className: PropTypes.string,
   flipX: PropTypes.bool,
   flipY: PropTypes.bool,
+  cta: PropTypes.shape({
+    icon: PropTypes.node,
+    title: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.array,
+    ]),
+    href: PropTypes.string,
+  }),
   preserveRatio: PropTypes.bool,
 };
 
@@ -43,4 +68,5 @@ Diagonal.defaultProps = {
   flipX: false,
   flipY: false,
   preserveRatio: false,
+  cta: {},
 };
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import ImagePrestationHosting from '../public/images/prestations/hebergement-opt
 import Diagonal from '../components/layouts/Diagonal';
 import ImageDiscoverTma from '../public/images/prestations/presentation-integration-web.jpeg';
 import ProjectItem from '../components/items/ProjectItem';
+import MoreIcon from '../components/icons/MoreIcon';
 
 export default function Home() {
   return (
@@ -109,39 +110,52 @@ export default function Home() {
         </div>
         <Diagonal
           className="-z-10"
-          bgClass="fill-gray-dark"
+          bgClass="fill-gray-darker"
           bgCorner="fill-orange"
         />
       </div>
 
-      <div className="container">
-        <SectionTitle
-          content="Consultez mes dernières créations, atteignant tous, l’esthétique du détail et de la fonctionnalité qui me démarque du reste en tant que développeur Freelance."
-          title="Projets"
-        />
+      <div className="bg-gray-darker">
+        <div className="container -mb-8">
+          <SectionTitle
+            content="Consultez mes dernières créations, atteignant tous, l’esthétique du détail et de la fonctionnalité qui me démarque du reste en tant que développeur Freelance."
+            title="Projets"
+          />
 
-        <div className="mt-3 sm:mt-0 grid gap-2 sm:gap-0 grid-cols-2 md:grid-cols-4">
-          <ProjectItem
-            image="/images/portfolio/biosalines-3d.jpeg"
-            title="Laboratoires Biosalines"
-            category="WordPress"
-          />
-          <ProjectItem
-            image="/images/portfolio/biosalines-3d.jpeg"
-            title="Laboratoires Biosalines"
-            category="WordPress"
-          />
-          <ProjectItem
-            image="/images/portfolio/biosalines-3d.jpeg"
-            title="Laboratoires Biosalines"
-            category="WordPress"
-          />
-          <ProjectItem
-            image="/images/portfolio/biosalines-3d.jpeg"
-            title="Laboratoires Biosalines"
-            category="WordPress"
-          />
+          <div className="mt-3 sm:mt-0 grid gap-2 sm:gap-0 grid-cols-2 md:grid-cols-4">
+            <ProjectItem
+              image="/images/portfolio/biosalines-3d.jpeg"
+              title="Laboratoires Biosalines"
+              category="WordPress"
+            />
+            <ProjectItem
+              image="/images/portfolio/biosalines-3d.jpeg"
+              title="Laboratoires Biosalines"
+              category="WordPress"
+            />
+            <ProjectItem
+              image="/images/portfolio/biosalines-3d.jpeg"
+              title="Laboratoires Biosalines"
+              category="WordPress"
+            />
+            <ProjectItem
+              image="/images/portfolio/biosalines-3d.jpeg"
+              title="Laboratoires Biosalines"
+              category="WordPress"
+            />
+          </div>
         </div>
+
+        <Diagonal
+          className="-z-10"
+          bgClass="fill-gray-dark"
+          bgCorner="fill-orange"
+          cta={{
+            icon: <MoreIcon />,
+            title: ['Voir tous', <br />, 'les articles'],
+            href: '/',
+          }}
+        />
       </div>
     </div>
   );
