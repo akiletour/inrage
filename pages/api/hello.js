a270421de3f92f0fa18c0ee6d5d9bcdc956d0ef8
Pascal GAULT <pascal@inrage.fr>
2021-12-21 12:48:36 +0100

    feat(api): check akismet post and send through mailjet

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,75 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { AkismetClient } from 'akismet-api';
+import Mailjet from 'node-mailjet';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' });
+export default async function handler(req, res) {
+  const blog = 'https://www.inrage.fr';
+  const client = new AkismetClient({ key: process.env.AKISMET_API_KEY, blog });
+
+  const forwarded = req.headers['x-forwarded-for'];
+  const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
+
+  try {
+    const isValid = await client.verifyKey();
+
+    if (!isValid) {
+      res.status(500);
+      return res;
+    }
+  } catch (err) {
+    res.status(500);
+    return res;
+  }
+
+  const {
+    email, firstname, lastname, content,
+  } = req.body;
+
+  const comment = {
+    ip,
+    useragent: req.headers['user-agent'] || '',
+    content,
+    email,
+    name: `${firstname} ${lastname}`,
+  };
+
+  try {
+    const isSpam = await client.checkSpam(comment);
+
+    if (isSpam) {
+      res.status(404).json({ msg: 'spam' });
+      return res;
+    }
+  } catch (err) {
+    res.status(500);
+    return res;
+  }
+
+  const mailjet = Mailjet.connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
+
+  const request = mailjet
+    .post('send', { version: 'v3.1' })
+    .request({
+      Messages: [{
+        From: {
+          Email: 'pascal@inrage.fr',
+          Name: 'Pascal GAULT',
+        },
+        To: [{
+          Email: 'pascal@inrage.fr',
+          Name: 'Pascal GAULT',
+        }],
+        Subject: '[inRage] Formulaire de contact',
+        HTMLPart: content,
+      }],
+    });
+  request
+    .then(() => {
+      res.status(200).json({ name: 'John Doe3' });
+    })
+    .catch(() => {
+      res.status(500);
+      return res;
+    });
+
+  return res;
 }
