cb455a9ecdc5a832a038bc309d02ebc4cc6f1517
Pascal GAULT <pascal@inrage.fr>
2021-12-24 00:46:56 +0100

    chore(ci): fix eslint quote

--- components/items/ArticleItem.js
+++ components/items/ArticleItem.js
@@ -1,7 +1,7 @@
 export default function ArticleItem() {
   return (
     <div>
-      <h3>Titre de l'article</h3>
+      <h3>Titre de l&apos;article</h3>
       <div>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, id iste natus nobis non
         placeat quidem reiciendis sit! Aliquam blanditiis, quam. Aliquid autem dolorem eum rem sequi
