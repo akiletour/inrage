bb3bd72c0059bc1a36c37888982e039c2edc8b59
Pascal GAULT <pascal@inrage.fr>
2021-12-26 22:22:48 +0100

    fix(article): remove global link and add it to image

--- components/items/ArticleItem.js
+++ components/items/ArticleItem.js
@@ -5,18 +5,20 @@ import ButtonLink from '../ButtonLink';
 
 export default function ArticleItem({ image, title, excerpt }) {
   return (
-    <Link href="/">
-      <a className="relative flex flex-col">
-        <div className="absolute top-1 w-6 text-center left-1 z-10 bg-white">
-          <div className="py-1 bg-orange font-medium text-gray-darker text-2xl">26</div>
-          <div className="text-gray-darker">OCT</div>
-        </div>
-        <Image src={image} width={595} height={265} alt={title} />
-        <h3 className="text-white text-2xl font-medium mt-1">{title}</h3>
-        <p className="mt-1 mb-2">{excerpt}</p>
-        <ButtonLink href="/">Lire la suite</ButtonLink>
-      </a>
-    </Link>
+    <div className="relative flex flex-col">
+      <div className="absolute top-1 w-6 text-center left-1 z-10 bg-white">
+        <div className="py-1 bg-orange font-medium text-gray-darker text-2xl">26</div>
+        <div className="text-gray-darker">OCT</div>
+      </div>
+      <Link href="/">
+        <a>
+          <Image src={image} width={595} height={265} alt={title} />
+        </a>
+      </Link>
+      <h3 className="text-white text-2xl font-medium mt-1">{title}</h3>
+      <p className="mt-1 mb-2">{excerpt}</p>
+      <ButtonLink href="/">Lire la suite</ButtonLink>
+    </div>
   );
 }
 
