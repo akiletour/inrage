f8d9ba251bdf7b953735574435b57b9123fe3934
Pascal GAULT <pascal@inrage.fr>
2021-12-26 20:51:49 +0100

    feat(article-item): optimize color of the month

--- components/items/ArticleItem.js
+++ components/items/ArticleItem.js
@@ -9,7 +9,7 @@ export default function ArticleItem({ image, title, excerpt }) {
       <a className="relative flex flex-col">
         <div className="absolute top-1 w-6 text-center left-1 z-10 bg-white">
           <div className="py-1 bg-orange font-medium text-gray-darker text-2xl">26</div>
-          <div>OCT</div>
+          <div className="text-gray-darker">OCT</div>
         </div>
         <Image src={image} width={595} height={265} alt={title} />
         <h3 className="text-white text-2xl font-medium mt-1">{title}</h3>
