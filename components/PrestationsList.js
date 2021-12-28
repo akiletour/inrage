26e5f0d678dc4e80f4e90c406759309ecdca528f
Pascal GAULT <pascal@inrage.fr>
2021-12-28 16:34:36 +0100

    feat(prestationsList): add prestation entry link

--- components/PrestationsList.js
+++ components/PrestationsList.js
@@ -9,7 +9,7 @@ export default function PrestationsList() {
       <PrestationItem
         image={ImagePrestationBuildWebsite}
         title={['Conception / Refonte', <br />, 'de sites Internet']}
-        link="/"
+        link="/prestations"
         linkText="Découvrir mes prestations"
       >
         Sites vitrines, associatifs, e-commerce ou dédiés.
