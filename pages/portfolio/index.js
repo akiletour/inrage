fb7f0d9d3708a44b38bd2741cdcdec1cdc2942af
Pascal GAULT <pascal@inrage.fr>
2021-12-28 23:20:40 +0100

    feat(portfolio): add metadatas to the page

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import Head from 'next/head';
 import SectionTitle from '../../components/SectionTitle';
 import { getPortfolioProjects } from '../../lib/api';
 import ProjectItem from '../../components/items/ProjectItem';
@@ -6,6 +7,11 @@ import ProjectItem from '../../components/items/ProjectItem';
 export default function Portfolio({ projects: { edges } }) {
   return (
     <div className="container">
+      <Head>
+        <title>Portfolio des projets de création de site Internet</title>
+        <meta name="description" content="Retrouvez la liste des projets de création de site web, de boutique e-commerce ou encore d'application web" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <SectionTitle
         content={"Pour toute demande ou devis, n'hésitez pas à nous contacter en remplissant le formulaire ci-dessous, nous serons ravis de vous répondre."}
         title="Portfolio"
