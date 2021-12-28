5a79cacb4c34c1b4c07cc8cb3e51bcb135715d0f
Pascal GAULT <pascal@inrage.fr>
2021-12-28 17:25:25 +0100

    feat(page): add build website prestation page

--- pages/prestations/creation-site-web/index.js
+++ pages/prestations/creation-site-web/index.js
@@ -1,11 +1,18 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import Head from 'next/head';
 import Diagonal from '../../../components/layouts/Diagonal';
 import SectionTitle from '../../../components/SectionTitle';
+import ContactForm from '../../../components/ContactForm';
 
 export default function PrestationBuildWebsite() {
   return (
     <div>
+      <Head>
+        <title>Création de site internet | Freelance création de site web - inRage</title>
+        <meta name="description" content="Développeur et intégrateur freelance, spécialisé dans la création de sites internet sur WordPress, Prestashop et Symfony. Découvrez toutes mesprestations Web !" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="flex relative container mt-6 lg:-mt-12">
         <div className="hidden lg:block overflow-hidden absolute lg:right-[60%]">
           <div className="w-[697px] h-[736px] relative">
@@ -55,7 +62,7 @@ export default function PrestationBuildWebsite() {
           bgCorner="fill-gray-darker"
         />
         <div className="container text-center">
-          <div className="px-18 text-lg">
+          <div className="md:px-18 text-lg">
             <h2 className="text-white text-4xl font-bold mb-6">
               Un processus de développement
               <br />
@@ -87,31 +94,38 @@ export default function PrestationBuildWebsite() {
         />
       </div>
 
-      <div className="container">
+      <div className="container -mb-10">
         <SectionTitle
           title="Services"
           content="L'utilisation de CMS aussi populaires que Prestashop et Wordpress, disposant de communauté active et importante, vous garanti d'avoir d'une solution qui pourra évoluer au fur et à mesure de vos besoins et des évolutions technologiques."
         />
 
-        <div className="flex">
-          <div className="w-2/5">Left</div>
-          <div className="w-3/5">
-            <h3>
+        <div className="flex flex-col md:flex-row items-center mt-6">
+          <div className="md:w-2/5">
+            <Image
+              src="/images/website-wp.png"
+              width={508}
+              height={464}
+              alt="CMS WordPress"
+            />
+          </div>
+          <div className="md:w-3/5 md:pl-4">
+            <h3 className="text-white text-4xl font-bold mb-3">
               Développement site vitrine
               <br />
               avec WordPress
             </h3>
 
-            Wordpress est un CMS simple et rapide d’accès, c’est mon CMS de prédilection !
-            <p>
+            <p>Wordpress est un CMS simple et rapide d’accès, c’est mon CMS de prédilection !</p>
+            <p className="mt-2">
               Idéal pour la création de site vitrine, Wordpress permet de développer rapidement des
               sites entièrement administrables.
             </p>
-            <p>
+            <p className="mt-2">
               C’est un outil open source qui permet également la personnalisation avancée de son
               backoffice afin qu’il réponde le plus efficacement à vos besoins.
             </p>
-            <p>
+            <p className="mt-2 mb-3">
               De plus, Wordpress dispose d’une communauté importante et investie. Ainsi, de
               nombreux plugins et fonctionnalités sont existants, ils permettent de faire évoluer
               votre site au gré de vos besoins.
@@ -121,6 +135,143 @@ export default function PrestationBuildWebsite() {
             </Link>
           </div>
         </div>
+
+        <div className="flex items-center mt-8 flex-col md:flex-row-reverse">
+          <div className="md:w-2/5">
+            <Image
+              src="/images/website-prestashop.png"
+              width={448}
+              height={375}
+              alt="CMS Prestashop"
+            />
+          </div>
+          <div className="md:w-3/5 md:pr-4 md:text-right">
+            <h3 className="text-white text-4xl font-bold mb-3">
+              Développement site e-commerce
+              <br />
+              avec Prestashop
+            </h3>
+
+            <p className="mt-2">
+              Prestashop est le CMS phare dans la création de boutique en
+              ligne.
+            </p>
+            <p className="mt-2">
+              A la manière de Wordpress, la création de site prestashop permet de
+              développer rapidement un outil de e-commerce entièrement administrable en toute
+              autonomie.
+            </p>
+            <p className="mt-2 mb-3">
+              C’est également un outil open source qui dispose d’une
+              communauté solide.
+            </p>
+
+            <Link href="/">
+              <a className="button">Développer avec WordPress</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <Diagonal
+        bgCorner="fill-orange"
+        bgClass="fill-gray-darker"
+      />
+
+      <div className="bg-gray-darker">
+        <div className="container">
+          <SectionTitle
+            content="inRage est une agence créative et innovante basée au coeur de La Rochelle. Spécialisée dans la création de site Web."
+            title="En savoir plus"
+          />
+
+          <div className="flex items-center flex-col md:flex-row mt-10">
+            <div className="md:w-2/5">
+              <Image
+                src="/images/sketch_logo.png"
+                width={394}
+                height={356}
+                alt="Création et intégration avec Sketch"
+              />
+            </div>
+            <div className="md:w-3/5 md:pl-4">
+              <h3 className="text-white text-4xl font-bold mb-3">
+                Création et intégration avec Sketch
+              </h3>
+
+              <p>
+                Pour la création graphiques des maquettes de l’ensemble de mes projets digitaux,
+                j’utilise le logiciel Sketch. Ce logiciel, très complet, fait le lien entre le web
+                designer et le développeur.
+              </p>
+              <p className="mt-2">
+                Une des forces de Sketch est son approche 100% vectorielle : pratique, simple et
+                rapide.
+              </p>
+              <p className="mt-2">
+                Sketch est conçu pour créer des UI : C’est à dire, tout ce que vous avez créer sur
+                cet outil est réalisable en HTML/CSS. De plus, une fonctionnalité de générateur de
+                codes est même disponible.
+              </p>
+              <p className="mt-2">
+                Enfin, une autre fonctionnalité permet d’exporter les assets d’un projet. En
+                d’autres termes, grâce à sketch, le développeur dispose de l’ensemble des
+                informations nécessaires pour intégrer la maquette d’un site.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center flex-col md:flex-row-reverse mt-10 md:text-right">
+            <div className="md:w-2/5">
+              <Image
+                src="/images/devops.png"
+                width={474}
+                height={220}
+                alt="Intégration et développement continu"
+              />
+            </div>
+            <div className="md:w-3/5 md:pr-4">
+              <h3 className="text-white text-4xl font-bold mb-3">
+                Intégration et déploiement continu
+              </h3>
+
+              <p>
+                L&apos;intégration continue consiste à segmenter le développement d’un site internet
+                en différents blocs ou fonctionnalités. Ainsi, dès qu’une fonctionnalité est
+                terminée, elle est implémentée sur un environnement de test afin d’en vérifier le
+                bon fonctionnement. Le principal but de cette pratique est de détecter les problèmes
+                d&apos;intégration au plus tôt lors du développement.
+              </p>
+
+              <p className="mt-2">
+                De plus, elle permet d&apos;automatiser l&apos;exécution des tests et de voir
+                l&apos;évolution du développement du site internet. Cette méthode de travail est
+                également largement plébiscitée pour les projets nécessitant l’intervention de
+                plusieurs développeurs.
+              </p>
+
+              <p className="mt-2">
+                Cette méthode est efficace lorsqu’elle est associée à une autre méthode : le
+                déploiement continu. Cette dernière permet à mes clients de consulter les
+                modifications apportées à leur site en temps réel sur mon serveur de préproduction.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <Diagonal
+          bgCorner="fill-orange"
+          bgClass="fill-gray-dark"
+        />
+      </div>
+
+      <div className="container">
+        <SectionTitle
+          content={"Pour toute demande ou devis, n'hésitez pas à me contacter en remplissant le formulaire ci-dessous, je serais ravis de vous répondre."}
+          title="Contact"
+        />
+
+        <ContactForm />
       </div>
     </div>
   );
