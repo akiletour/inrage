451a81386436ba2dd761a43ebcc00ef62f376823
Pascal GAULT <pascal@inrage.fr>
2021-12-31 14:34:18 +0100

    fix(component): allow SectionTitle to have array title

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -1,13 +1,21 @@
+import { Fragment } from 'react';
+
 type Props = {
   className?: string,
   content: string,
-  title: string,
+  title: string | Array<string | JSX.Element>,
 }
 
 export default function SectionTitle({ className = '', title, content }: Props) {
   return (
     <div className={`flex md:items-center flex-col md:flex-row ${className}`}>
-      <h2 className="text-white flex-none text-3xl font-bold max-w-[390px]">{title}</h2>
+      <h2 className="text-white flex-none text-3xl font-bold max-w-[390px]">
+        {Array.isArray(title) ? title.map((ttl) => (
+          <Fragment key={ttl.toString()}>
+            {ttl}
+          </Fragment>
+        )) : title}
+      </h2>
       <p className="mt-2 md:mt-0 text-gray-light flex-1 flex items-center">
         <span className="hidden md:block w-[2px] h-[120px] mx-8 bg-gray flex-none -skew-x-[30deg]" />
         {content}
