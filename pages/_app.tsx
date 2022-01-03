367b067b497670074b3cd0908345a9ff396a8915
Pascal GAULT <pascal@inrage.fr>
2022-01-03 23:28:44 +0100

    Install and add default seo configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,29 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
 import Header from '../components/layouts/Header';
 import Footer from '../components/layouts/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo
+        additionalLinkTags={[{
+          rel: 'icon',
+          href: '/favicon.png',
+        }]}
+        openGraph={{
+          type: 'website',
+          locale: 'fr_FR',
+          url: 'https://www.inrage.fr/',
+          site_name: 'inRage',
+        }}
+        twitter={{
+          handle: '@akiletour',
+          site: '@akiletour',
+          cardType: 'summary_large_image',
+        }}
+      />
       <Header pageTitle={pageProps.pageTitle || ''} breadcrumb={pageProps.breadcrumb} />
       <Component {...pageProps} />
       <Footer />
