8d5b34535c2fa3980ce195389a088cb4627644ec
Pascal GAULT <pascal@inrage.fr>
2022-01-12 01:06:20 +0100

    Retrieve post comments with GraphQL

--- lib/blog.ts
+++ lib/blog.ts
@@ -26,6 +26,7 @@ export async function getSingleArticle(slug: string) {
   const data = await fetchAPI(`
    query getSingleArticle($id: ID!) {
      post(id: $id, idType: SLUG) {
+       id
        title
        slug
        date
@@ -63,4 +64,31 @@ export async function BlogPosts(max: number = 1000) {
   });
 
   return data?.posts;
+}
+
+export async function fetchBlogComments(slug: string) {
+  const data = await fetchAPI(`
+    query fetchBlogComments($id: ID!) {
+      comments(where: { contentId: $id, contentType: POST }) {
+        edges {
+          node {
+            id
+            content
+            author {
+              node {
+                name
+              }
+            }
+            dateGmt
+          }
+        }
+      }
+    }
+  `, {
+    variables: {
+      id: slug,
+    },
+  });
+
+  return data?.comments?.edges || [];
 }
