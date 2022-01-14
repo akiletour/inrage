e6ed55f75b52f8ba5ffae75cb9dc60e6f246b777
Pascal GAULT <pascal@inrage.fr>
2022-01-14 23:56:29 +0100

    Create a custom 404 page #24

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import Button from '@component/Button';
+import NoScrollLink from '@component/NoScrollLink';
+import Layout from '@component/layouts/Layout';
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <div className="text-center">
+        <h1 className="text-white text-9xl font-black">404</h1>
+        <p className="text-xl">LA PAGE QUE VOUS RECHERCHEZ EST INTROUVABLE.</p>
+
+        <NoScrollLink href="/">
+          <a className="button mt-4">Retour à l&apos;accueil</a>
+        </NoScrollLink>
+      </div>
+    </Layout>
+  );
+}
