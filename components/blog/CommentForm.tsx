b53779260c90d3bb9302c17bb1e832b08c617940
Pascal GAULT <pascal@inrage.fr>
2022-01-17 23:58:06 +0100

    feat(comment): create a reducer, and handle form submission

--- components/blog/CommentForm.tsx
+++ components/blog/CommentForm.tsx
@@ -1,7 +1,9 @@
+import { useReducer } from 'react';
+
 import { useForm } from 'react-hook-form';
 
-import { inputClassname } from '@component/ContactForm';
 import Button from '@component/Button';
+import { inputClassname } from '@component/ContactForm';
 
 type FormData = {
   name: string;
@@ -9,18 +11,86 @@ type FormData = {
   content: string;
 };
 
+type StateType = {
+  error: string;
+  success: boolean;
+  loading: boolean;
+};
+
+type Action =
+  | { type: 'error'; error: string }
+  | { type: 'submit' }
+  | { type: 'success' };
+
+const initialState = { error: '', success: false, loading: false };
+
+function reducer(state: StateType, action: Action) {
+  switch (action.type) {
+    case 'submit':
+      return {
+        ...state,
+        loading: true,
+        error: '',
+        success: false,
+      };
+    case 'error':
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+        success: false,
+      };
+    case 'success':
+      return {
+        ...state,
+        loading: false,
+        error: '',
+        success: true,
+      };
+    default:
+      return state;
+  }
+}
+
 export default function CommentForm() {
+  const [formState, dispatch] = useReducer(reducer, initialState);
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<FormData>();
   const onSubmit = (data: FormData) => {
-    console.log('submitting form', data);
+    dispatch({ type: 'submit' });
+    fetch('/api/post-comment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((r) => {
+        if (r.status !== 200) {
+          throw new Error(
+            'Désolé, une erreur est survenue lors de la soumission du formulaire'
+          );
+        }
+
+        return r.json();
+      })
+      .then(() => {
+        dispatch({ type: 'success' });
+        reset();
+      })
+      .catch((err: any) => {
+        dispatch({ type: 'error', error: err.message });
+      });
   };
   return (
     <div className="mt-8">
       <form onSubmit={handleSubmit(onSubmit)}>
+        {formState.error && <p>{formState.error}</p>}
         <div className="relative">
           <input
             className={`${inputClassname} ${
@@ -67,10 +137,20 @@ export default function CommentForm() {
           )}
         </div>
 
-        <div className="mt-2">
-          <Button submit className="button py-3 px-4 w-full md:w-auto">
+        <div className="mt-2 flex items-center">
+          <Button
+            isLoading={formState.loading}
+            submit
+            className="button py-3 px-4 w-full md:w-auto"
+          >
             Envoyer mon message
           </Button>
+
+          {formState.success && (
+            <p className="text-[#27ae60] font-medium ml-4">
+              Merci pour votre commentaire.
+            </p>
+          )}
         </div>
       </form>
     </div>
