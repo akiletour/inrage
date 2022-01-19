6d41b3a9dbcf9eeefa3ebfeb50bb91d7ad570c7a
Pascal GAULT <pascal@inrage.fr>
2022-01-19 01:01:25 +0100

    feat(blog): display 2 related blog post

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -4,8 +4,11 @@ import { useRouter } from 'next/router';
 
 import PostBody from '@component/blog/PostBody';
 import PostComments from '@component/blog/PostComments';
+import ArticleItem from '@component/items/ArticleItem';
 import Layout from '@component/layouts/Layout';
+import SectionTitle from '@component/SectionTitle';
 import { getAllArticlesWithSlug, getSingleArticle } from '@lib/blog';
+import { BlogItem } from '@type/blog';
 
 type BlogFullArticleType = {
   id: string;
@@ -22,9 +25,10 @@ type BlogFullArticleType = {
 
 type BlogType = {
   post: BlogFullArticleType;
+  relatedArticles: Array<{ node: BlogItem }>;
 };
 
-export default function BlogDetail({ post }: BlogType) {
+export default function BlogDetail({ post, relatedArticles }: BlogType) {
   const router = useRouter();
 
   if (!router.isFallback && !post?.slug) {
@@ -46,16 +50,45 @@ export default function BlogDetail({ post }: BlogType) {
         <PostBody content={post?.content || ''} />
       </div>
       {post?.id && <PostComments id={post?.postId} />}
+
+      <div className="container">
+        <SectionTitle
+          content="Retrouvez ci-dessous quelques articles qui pourrait vous intéresser."
+          title="Articles reliés"
+        />
+        <div className="grid md:grid-cols-2 gap-4 -mb-8 mt-6">
+          {relatedArticles.map(
+            ({ node: { slug, title, featuredImage, excerpt, date } }) => (
+              <div key={slug}>
+                <ArticleItem
+                  slug={slug}
+                  featuredImage={featuredImage}
+                  title={title}
+                  date={date}
+                  excerpt={excerpt}
+                />
+              </div>
+            )
+          )}
+        </div>
+      </div>
     </Layout>
   );
 }
 
 export async function getStaticProps({ params }: { params: { slug: string } }) {
-  const { post } = await getSingleArticle(params.slug);
+  const { post, posts } = await getSingleArticle(params.slug);
+
+  const firstArticle =
+    posts.edges[Math.floor(Math.random() * posts.edges.length)];
+  const secondArticle =
+    posts.edges[Math.floor(Math.random() * posts.edges.length)];
+
   return {
     props: {
       pageTitle: post.title,
       post,
+      relatedArticles: [firstArticle, secondArticle],
       breadcrumb: [
         {
           title: 'Blog',
