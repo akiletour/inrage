f0d64a24070731288c503c77f1ef0441cc19a7ca
Pascal GAULT <pascal@inrage.fr>
2022-01-23 22:25:50 +0100

    test(cypress): use typescript in cypress files

--- cypress/integration/portfolio.tsx
+++ cypress/integration/portfolio.tsx
@@ -1,10 +1,8 @@
-
 import { checkEmptyLinks } from '../support/commands';
 
 /// <reference types="cypress"/>
 
 context('Public Portfolio list page', () => {
-  
   beforeEach(() => {
     cy.visit('http://localhost:3000/portfolio');
   });
@@ -15,10 +13,9 @@ context('Public Portfolio list page', () => {
     cy.get('.project-item').should('not.contain', 'Audilab');
   });
 });
-
 context('Private Portfolio list page', () => {
   checkEmptyLinks();
-  
+
   it('Should contain a private project', () => {
     cy.visit('http://localhost:3000/api/preview?secret=secret_preview');
     cy.get('.project-item').should('contain', 'Audilab');
@@ -31,6 +28,26 @@ context('Private Portfolio list page', () => {
     cy.visit('http://localhost:3000/api/exit-preview');
     cy.visit('http://localhost:3000/portfolio');
     cy.get('.project-item').should('not.contain', 'Audilab');
-  })
+  });
+});
+
+context('Public project detail', () => {
+  it('should not access to a private project', () => {
+    cy.visit(
+      'http://localhost:3000/portfolio/wordpress/centre-auditif-audilab'
+    );
+    cy.get('body').should('not.contain', 'Audilab').should('contain', '404');
+  });
+
+  it('should access to the private project', () => {
+    cy.visit('http://localhost:3000/api/preview?secret=secret_preview');
+
+    cy.visit(
+      'http://localhost:3000/portfolio/wordpress/centre-auditif-audilab'
+    );
+    cy.get('body')
+      .should('contain', 'Centre auditif Audilab')
+      .should('not.contain', '404');
+  });
 });
 export {};
