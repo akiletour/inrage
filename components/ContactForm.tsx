29e92d7724e724dba51d96e90694241062a2c6d5
Pascal GAULT <pascal@inrage.fr>
2022-01-25 08:26:48 +0100

    fix(contact): fire hello slack after sending the mail

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -30,14 +30,6 @@ export default function ContactForm({ lg = false }: Props) {
   const onSubmit = (data: FormData) => {
     setLoading(true);
     setSuccess(false);
-    fetch('/api/hello-slack', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    });
 
     fetch('/api/hello', {
       method: 'POST',
@@ -52,6 +44,13 @@ export default function ContactForm({ lg = false }: Props) {
         setLoading(false);
         setSuccess(true);
         reset();
+        fetch('/api/hello-slack', {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
       });
   };
 
