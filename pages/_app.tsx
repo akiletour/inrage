50d2db236244610dc12156360cce8dcfa5a55846
Pascal GAULT <pascal@inrage.fr>
2022-01-28 01:18:14 +0100

    feat(seo): add organization json-ld for inRage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import { AnimatePresence } from 'framer-motion';
-import { DefaultSeo } from 'next-seo';
+import { DefaultSeo, OrganizationJsonLd } from 'next-seo';
 import { AppProps } from 'next/app';
 
 import Footer from '@component/layouts/Footer';
@@ -38,6 +38,35 @@ function MyApp({ Component, pageProps, router }: AppProps) {
           site: '@akiletour',
           cardType: 'summary_large_image',
         }}
+      />
+      <OrganizationJsonLd
+        organizationType="Corporation"
+        id="https://www.inrage.fr/#corporation"
+        logo="https://i1.wp.com/www.inrage.fr/wp-content/uploads/2019/12/logo-inrage.png?fit=150%2C56&ssl=1"
+        legalName="inRage SARL"
+        name="inRage"
+        address={{
+          streetAddress: '10 rue Jean Perrin',
+          addressLocality: 'La Rochelle',
+          addressRegion: 'Charente Maritime',
+          postalCode: '17000',
+          addressCountry: 'FR',
+        }}
+        contactPoints={[
+          {
+            telephone: '+33 (0)6 51 89 89 17',
+            contactType: 'Pascal GAULT',
+            areaServed: 'FR',
+            availableLanguage: 'French',
+          },
+        ]}
+        url="https://www.inrage.fr"
+        sameAs={[
+          'https://www.facebook.com/inragefr/',
+          'https://www.instagram.com/akiletour/',
+          'https://www.linkedin.com/in/akiletour/',
+          'https://twitter.com/akiletour',
+        ]}
       />
       <Header
         pageTitle={pageProps.pageTitle || ''}
