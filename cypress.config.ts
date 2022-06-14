f352d40b8d214275a66a6464dac07e391c383520
Pascal GAULT <pascal@inrage.fr>
2022-06-15 00:21:16 +0200

    fix(test): upgrade cypress configuration for Cypress 10

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
+  viewportWidth: 1440,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
