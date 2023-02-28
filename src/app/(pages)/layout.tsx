9747ad6b9d225a891c445376ff4fa017c581af26
Pascal GAULT <pascal@inrage.fr>
2023-02-28 23:06:24 +0100

    chore: prepare app folder structure

---
+++ src/app/(pages)/layout.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { ReactNode } from 'react';
+
+import Footer from '@layout/Footer';
+
+import './globals.css';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="fr">
+      <head />
+      <body>
+        <div>{children}</div>
+        <Footer />
+      </body>
+    </html>
+  );
+}
