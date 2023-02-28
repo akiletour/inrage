53423099a169545ebb9d9e166c44cf1909de0e74
Pascal GAULT <pascal@inrage.fr>
2023-02-28 23:17:47 +0100

    feat(hp): create a blog component to retrieve latest articles

--- src/app/types/graphql.ts
+++ src/app/types/graphql.ts
@@ -19,3 +19,22 @@ export interface ProjectList {
     }>;
   };
 }
+
+export interface ArticleList {
+  title: string;
+  slug: string;
+  date: string;
+  excerpt: string;
+  featuredImage: FeaturedImageNode;
+}
+
+export interface List<T> {
+  data: Record<
+    string,
+    {
+      edges: Array<{
+        node: T;
+      }>;
+    }
+  >;
+}
