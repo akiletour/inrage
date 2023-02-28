affe6b455c49932cc62afa2cd333422f74d80df1
Pascal GAULT <pascal@inrage.fr>
2023-02-28 23:15:22 +0100

    feat(hp): create portfolio component to retrieve latest projects

---
+++ src/app/types/graphql.ts
@@ -0,0 +1,21 @@
+type FeaturedImageNode = {
+  node: {
+    sourceUrl: string;
+  };
+};
+
+export interface ProjectList {
+  id: string;
+  title: string;
+  slug: string;
+  status: 'publish' | 'draft' | 'private' | 'future';
+  featuredImage: FeaturedImageNode;
+  supports: {
+    edges: Array<{
+      node: {
+        name: string;
+        slug: string;
+      };
+    }>;
+  };
+}
