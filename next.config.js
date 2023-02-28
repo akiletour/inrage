559797f28ea734b000928adc58bc6f6c1c1f883d
Pascal GAULT <pascal@inrage.fr>
2023-02-28 23:02:43 +0100

    chore(deps): Upgrade to NextJS 13 and app directory

--- next.config.js
+++ next.config.js
@@ -12,6 +12,9 @@ const nextConfig = {
   images: {
     domains: ['i2.wp.com', 'i0.wp.com', 'i1.wp.com'],
   },
+  experimental: {
+    appDir: true,
+  },
   i18n: {
     locales: ['fr'],
     defaultLocale: 'fr',
