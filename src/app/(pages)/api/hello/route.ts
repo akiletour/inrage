33e27715970a3cfb0565ed0d8dfcc2f29d31b66a
Pascal GAULT <pascal@inrage.fr>
2023-03-03 14:01:47 +0100

    feat(api): check contact spam submission with Akismet
    Related to: INR-6

--- src/app/(pages)/api/hello/route.ts
+++ src/app/(pages)/api/hello/route.ts
@@ -1,11 +1,31 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+import { isAkismetSpam } from '@util/akismet';
+
 export async function POST(request: NextRequest) {
   try {
     const { email, name, content, phone } = await request.json();
 
+    if (!email || !name || !content || !phone) {
+      throw new Error('Missing parameters');
+    }
+
+    // Check if the request is a spam
+    const isSpam = await isAkismetSpam(request, name, email, content);
+
+    if (isSpam) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Message marked as spam' }),
+        {
+          status: 422,
+        }
+      );
+    }
+
     return NextResponse.json({ success: true, message: 'Message sent' });
   } catch (error) {
-    return NextResponse.json({ success: false, message: 'Bad request' });
+    return new NextResponse(JSON.stringify({ message: 'Bad request' }), {
+      status: 400,
+    });
   }
 }
--- src/app/types/environment.d.ts
+++ src/app/types/environment.d.ts
@@ -3,6 +3,7 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production';
       WORDPRESS_API_URL: string;
+      AKISMET_API_KEY: string;
     }
   }
 }
