cc8077295af0940399934298c98d46d88467dec6
Pascal GAULT <pascal@inrage.fr>
2023-03-28 23:24:56 +0200

    feat(env): split WordPress API URL with a Backend URL

--- src/app/types/environment.d.ts
+++ src/app/types/environment.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production';
+      BACKEND_URL: string;
       WORDPRESS_API_URL: string;
       AKISMET_API_KEY: string;
 
