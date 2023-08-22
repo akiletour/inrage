0171ad3cb6f96ed03c6ad95af0663ccf28657add
Pascal GAULT <pascal@inrage.fr>
2023-08-23 00:46:20 +0200

    chore(stack): rearrange project files in src folder

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from '@component/NoScrollLink';
 import { RouteLink } from '@lib/route';
 
-import LogoWhite from '../../../public/logo-white.svg';
+import LogoWhite from '../../public/logo-white.svg';
 import Socials from '../components/Socials';
 
 export default function Footer() {
