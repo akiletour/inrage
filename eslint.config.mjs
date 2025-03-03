134f42210edee1fe57b2288d93bdcc53a3e887dc
Pascal GAULT <pascal@inrage.fr>
2025-03-03 11:56:51 +0100

    chore: remove vercel.json and add Prettier with ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,12 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
 })
-
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  }),
 ]
-
 export default eslintConfig
 
