f947e8ba6868c3a22f7145039d44af8a8ac93c33
Pascal GAULT <pascal@inrage.fr>
2025-09-30 10:04:08 +0200

    chore: rename postcss config file and update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,25 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
+  baseDirectory: __dirname,
 })
+
 const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
     ignores: [
       'node_modules/**',
-      'coverage/**',
       '.next/**',
       'out/**',
       'build/**',
       'next-env.d.ts',
     ],
   },
-  ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
-  }),
 ]
+
 export default eslintConfig
